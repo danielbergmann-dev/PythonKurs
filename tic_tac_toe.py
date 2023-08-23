2a9c0ba7ad62764a45b4c1e06c997eaf90d5a3fd
Daniel Bergmann <105534792+danielbergmann-dev@users.noreply.github.com>
2023-08-23 20:45:53 +0200

    Game Loop Tic Tac Toe

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -3,20 +3,39 @@ field = ["",
          "4", "5", "6",
          "7", "8", "9"]
 
+active_player = "X"
+run = True
+
 def print_field():
     print(field[1] + "|" + field[2] + "|" + field[3])
     print(field[4] + "|" + field[5] + "|" + field[6])
     print(field[7] + "|" + field[8] + "|" + field[9])
 
 def next_move():
+    global run
     while True:
-        player_move = int(input("Enter your move: "))
+        player_move = input("Enter your move: ")
+        if player_move == "q":
+            run = False
+            return
+        player_move = int(player_move)
         if 1 <= player_move <= 9:
-            return player_move
+            if field[player_move] == "X" or field[player_move] == "O":
+                print("Invalid move. Try again")
+            else:
+                return player_move
         else:
             print("Invalid move. Try again")
 
+def change_player():
+    global active_player
+    if active_player == "X":
+        active_player = "O"
+    else:
+        active_player = "X"
 
-
-
-next_move()
+while run:
+    print_field()
+    player_move = next_move()
+    field[player_move] = active_player
+    change_player()
