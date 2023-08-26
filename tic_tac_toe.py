f7a5453a4359e4131a3dcc39e060e6cc5d0d1cad
Daniel Bergmann <105534792+danielbergmann-dev@users.noreply.github.com>
2023-08-26 15:22:51 +0200

    Game Loop Tic Tac Toe

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -16,6 +16,7 @@ def next_move():
     while True:
         player_move = input("Enter your move: ")
         if player_move == "q":
+            print("Game aborted")
             run = False
             return
         player_move = int(player_move)
@@ -34,8 +35,58 @@ def change_player():
     else:
         active_player = "X"
 
+def check_win():
+    if field[1] == field[2] == field[3]:
+        return True
+    if field[4] == field[5] == field[6]:
+        return True
+    if field[7] == field[8] == field[9]:
+        return True
+    if field[1] == field[4] == field[7]:
+        return True
+    if field[2] == field[5] == field[8]:
+        return True
+    if field[3] == field[6] == field[9]:
+        return True
+    if field[1] == field[5] == field[9]:
+        return True
+    if field[3] == field[5] == field[7]:
+        return True
+    return False
+
+def check_draw():
+    for i in range(1, 9):
+        if field[i] != "X" and field[i] != "O":
+            return False
+    return True
+
+def game_repeat():
+    global run, field
+    newStart = input("Do you want to play again? (y/n): ")
+    if newStart.lower() == "y":
+        field = ["",
+                 "1", "2", "3",
+                 "4", "5", "6",
+                 "7", "8", "9"]
+        run = True
+    else:
+        run = False
+
+
 while run:
     print_field()
     player_move = next_move()
-    field[player_move] = active_player
-    change_player()
+    if player_move != None:
+        field[player_move] = active_player
+        if check_win():
+            print_field()
+            print("Player " + active_player + " wins!")
+            run = False
+
+        if check_draw():
+            print("Game over. It's a draw!")
+            run = False
+        change_player()
+    if not run:
+        game_repeat()
+
