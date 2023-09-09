26b1318f87fe37b3d754ec79018a5849fefbd942
Daniel Bergmann <dhetzer3@gmail.com>
2023-09-09 17:25:23 +0200

    ship movement

--- spaceinvaders/main.py
+++ spaceinvaders/main.py
@@ -8,17 +8,47 @@ class Game:
         pygame.display.set_caption("Space Invaders")
         self.clock = pygame.time.Clock()
         self.running = True
+        self.spaceship = Spaceship(self, 370, 515)
         self.background_img = pygame.image.load("img/stars.png")
 
         while self.running:
             self.clock.tick(60)
             self.screen.fill((0, 0, 255))
             self.screen.blit(self.background_img, (0, 0))
+            self.spaceship.update()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
 
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        self.spaceship.move(-15)
+                    if event.key == pygame.K_RIGHT:
+                        self.spaceship.move(15)
+
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_LEFT:
+                        self.spaceship.move(15)
+                    if event.key == pygame.K_RIGHT:
+                        self.spaceship.move(-15)
+
+
             pygame.display.update()
+
+class Spaceship:
+    def __init__(self, game, x, y):
+        self.game = game
+        self.x = x
+        self.y = y
+        self.spaceship_img = pygame.image.load("img/spaceship.png")
+        self.change_x = 0
+    def move(self, speed):
+        self.change_x += speed
+
+    def update(self):
+        self.x += self.change_x
+        print(self.x)
+        self.game.screen.blit(self.spaceship_img, (self.x, self.y))
 
 
 if __name__ == "__main__":
