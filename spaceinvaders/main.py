05a4f9f7586c72941f2619569a75299423c74a09
Daniel Bergmann <dhetzer3@gmail.com>
2023-09-09 11:43:06 +0200

    backround + FrameRate

--- spaceinvaders/main.py
+++ spaceinvaders/main.py
@@ -5,12 +5,20 @@ class Game:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((800, 600))
+        pygame.display.set_caption("Space Invaders")
+        self.clock = pygame.time.Clock()
         self.running = True
+        self.background_img = pygame.image.load("img/stars.png")
 
         while self.running:
+            self.clock.tick(60)
+            self.screen.fill((0, 0, 255))
+            self.screen.blit(self.background_img, (0, 0))
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
+
+            pygame.display.update()
 
 
 if __name__ == "__main__":
