b0363a2dd0b097a7f9433f1d608c052af2bca101
Daniel Bergmann <105534792+danielbergmann-dev@users.noreply.github.com>
2023-09-10 11:56:24 +0200

    spaceinvaders bullet

--- spaceinvaders/main.py
+++ spaceinvaders/main.py
@@ -25,12 +25,23 @@ class Game:
                         self.spaceship.move(-15)
                     if event.key == pygame.K_RIGHT:
                         self.spaceship.move(15)
+                    if event.key == pygame.K_SPACE:
+                        self.spaceship.fire_bullet()
 
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_LEFT:
                         self.spaceship.move(15)
                     if event.key == pygame.K_RIGHT:
                         self.spaceship.move(-15)
+
+
+            self.spaceship.update()
+            if len(self.spaceship.bullets) > 0:
+                for bullet in self.spaceship.bullets:
+                    if bullet.is_fired == True:
+                        bullet.update()
+                    else:
+                        self.spaceship.bullets.remove(bullet)
 
 
             pygame.display.update()
@@ -42,14 +53,43 @@ class Spaceship:
         self.y = y
         self.spaceship_img = pygame.image.load("img/spaceship.png")
         self.change_x = 0
+        self.bullets = []
+
+    def fire_bullet(self):
+        self.bullets.append(Bullet(self.game, self.x, self.y))
+        self.bullets[len(self.bullets) -1].fire()
+
+
     def move(self, speed):
         self.change_x += speed
 
     def update(self):
         self.x += self.change_x
-        print(self.x)
+        if self.x <= 0:
+            self.x = 0
+        elif self.x >= 736:
+            self.x = 736
         self.game.screen.blit(self.spaceship_img, (self.x, self.y))
+
+class Bullet:
+    def __init__(self, game, x, y):
+        self.game = game
+        self.x = x
+        self.y = y
+        self.bullet_img = pygame.image.load("img/bullet.png")
+        self.bullet_speed = 10
+
+    def fire(self):
+        self.is_fired = True
+
+    def update(self):
+        self.y -= self.bullet_speed
+        if self.y <= 0:
+            self.is_fired = False
+
+        self.game.screen.blit(self.bullet_img, (self.x, self.y))
 
 
 if __name__ == "__main__":
     game = Game()
+
