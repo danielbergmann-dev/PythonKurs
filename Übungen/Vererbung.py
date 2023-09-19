cb075ae84e55728881fe641d49678a9e5c13f620
Daniel Bergmann <dhetzer3@gmail.com>
2023-09-19 21:54:39 +0200

    Vererbung

---
+++ Übungen/Vererbung.py
@@ -0,0 +1,25 @@
+class Dog:
+    def __init__(selfself, name, age, color, fav_food):
+        self.name = name
+        self.age = age
+        self.color = color
+        self.fav_food = fav_food
+
+    def sleep(self):
+        print(" is sleeping")
+
+    def bark(self):
+        print(" is barking")
+
+class Cat:
+    def __init__(self, name, age, color, fav_food):
+        self.name = name
+        self.age = age
+        self.color = color
+        self.fav_food = fav_food
+
+    def sleep(self):
+        print(" is sleeping")
+
+    def meow(self):
+        print(" is meowing")
