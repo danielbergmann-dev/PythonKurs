60a755455550167acee41cf639591d0dbd3b7188
Daniel Bergmann <dhetzer3@gmail.com>
2023-09-20 21:19:32 +0200

    verrerbung

--- Übungen/Vererbung.py
+++ Übungen/Vererbung.py
@@ -6,24 +6,37 @@ class Animal:
         self.fav_food = fav_food
 
     def sleep(self):
-        print(" is sleeping")
+        print(f"{self.name} is sleeping")
 
     def move_fast(self):
-        print(" is moving fast")
+        print(f"{self.name} is moving fast")
 
 
 class Tiger(Animal):
     def move_fast(self):
-        print(" Tigers are moving very fast")
+        print("Tigers are moving very fast")
+
 
 class Dog(Animal):
+    def bark(self):
+        print(f"{self.name} is barking")
 
-   def bark(self):
-       print(" is barking")
 
 class Cat(Animal):
-   def meow(self):
-        print(" is meowing")
+    def meow(self):
+        print(f"{self.name} is meowing")
+
+
+class Owl(Animal):
+    def __init__(self, name, age, color, fav_food, hunting_time):
+        super().__init__(name, age, color, fav_food)
+        self.hunting_time = hunting_time
+
+    def sleep(self):
+        super().sleep()
+        print(f"{self.name} sleep during the day")
+        print(self.age)
+
 
 dog = Dog("Rex", 5, "brown", "meat")
 dog.bark()
@@ -31,11 +44,11 @@ dog.sleep()
 print(dog.name)
 
 cat = Cat("Miau", 3, "black", "fish")
-cat.meow()
-cat.sleep()
-print(cat.name)
-print(cat.fav_food)
-cat.move_fast()
 
 tiger = Tiger("Tiger", 10, "yellow", "meat")
 tiger.move_fast()
+
+owl = Owl("Owl", 2, "white", "mice", "night")
+owl.sleep()
+print(owl.name)
+print(owl.hunting_time)
