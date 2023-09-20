c0bff2f345955bb1d73925255922f87d60b83bc9
Daniel Bergmann <dhetzer3@gmail.com>
2023-09-20 15:15:16 +0200

    vererbung

--- Übungen/Vererbung.py
+++ Übungen/Vererbung.py
@@ -1,17 +1,4 @@
-class Dog:
-    def __init__(selfself, name, age, color, fav_food):
-        self.name = name
-        self.age = age
-        self.color = color
-        self.fav_food = fav_food
-
-    def sleep(self):
-        print(" is sleeping")
-
-    def bark(self):
-        print(" is barking")
-
-class Cat:
+class Animal:
     def __init__(self, name, age, color, fav_food):
         self.name = name
         self.age = age
@@ -21,5 +8,34 @@ class Cat:
     def sleep(self):
         print(" is sleeping")
 
-    def meow(self):
+    def move_fast(self):
+        print(" is moving fast")
+
+
+class Tiger(Animal):
+    def move_fast(self):
+        print(" Tigers are moving very fast")
+
+class Dog(Animal):
+
+   def bark(self):
+       print(" is barking")
+
+class Cat(Animal):
+   def meow(self):
         print(" is meowing")
+
+dog = Dog("Rex", 5, "brown", "meat")
+dog.bark()
+dog.sleep()
+print(dog.name)
+
+cat = Cat("Miau", 3, "black", "fish")
+cat.meow()
+cat.sleep()
+print(cat.name)
+print(cat.fav_food)
+cat.move_fast()
+
+tiger = Tiger("Tiger", 10, "yellow", "meat")
+tiger.move_fast()
